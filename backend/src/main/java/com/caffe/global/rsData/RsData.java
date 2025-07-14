962ea4240ca4c5ecaf4ba5d1eb899d0314d651fc
[BE8]윤지혜 <dooooood@khcu.ac.kr>
2025-07-14 17:49:15 +0900

    rsData 수정

--- backend/src/main/java/com/caffe/global/rsData/RsData.java
+++ backend/src/main/java/com/caffe/global/rsData/RsData.java
@@ -1,10 +1,14 @@
 package com.caffe.global.rsData;
 
 public record RsData<T>(
+        String resultCode,
         int statusCode,
         String msg,
         T data) {
-    public RsData(int statusCode, String msg) {
-        this(statusCode, msg, null);
+    public RsData(String resultCode, String msg) {
+        this(resultCode, msg, null);
+    }
+    public RsData(String resultCode, String msg, T data){
+        this(resultCode, Integer.parseInt(resultCode.split("-",2)[0]), msg, data);
     }
 }
