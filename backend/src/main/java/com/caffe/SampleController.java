b3c53b6c9f1e1d2a095278f7c4e487db89e82097
ahdeka <ahdeka01@gmail.com>
2025-07-14 16:48:55 +0900

    sample.html 추가

---
+++ backend/src/main/java/com/caffe/SampleController.java
@@ -0,0 +1,14 @@
+package com.caffe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "sample.html";
+    }
+
+}
