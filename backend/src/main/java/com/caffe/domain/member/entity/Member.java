0863436338c606e6a5de4bc420f16010ba9aff1a
ahdeka <ahdeka01@gmail.com>
2025-07-15 11:12:10 +0900

    ahdeka_BaseInitData 추가

--- backend/src/main/java/com/caffe/domain/member/entity/Member.java
+++ backend/src/main/java/com/caffe/domain/member/entity/Member.java
@@ -1,9 +1,6 @@
 package com.caffe.domain.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -28,6 +25,10 @@ public class Member {
 
     private String password;
     private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @CreatedDate
     private LocalDateTime createDate;
     @LastModifiedDate
