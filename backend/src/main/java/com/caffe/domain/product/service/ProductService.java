6c327824d99a4df4c71a8ff6a3de113337c57719
beekeeper24 <ahwnsk94@gmail.com>
2025-07-16 11:39:31 +0900

    예외처리 개선

--- backend/src/main/java/com/caffe/domain/product/service/ProductService.java
+++ backend/src/main/java/com/caffe/domain/product/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @Service
@@ -18,14 +19,18 @@ public class ProductService {
 
         // 상품 전체조회
         public List<Product> getAllProducts() {
-            return productRepository.findAll();
+            List<Product> products = productRepository.findAll();
+            if (products.isEmpty()) {
+                throw new NoSuchElementException("등록된 상품이 없습니다.");
+            }
+            return products;
         }
 
 
         //상품 단건조회
         public Product getProductById(int id) {
             return productRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException(id + "번 상품을 찾을 수 없습니다."));
+                    .orElseThrow(() -> new NoSuchElementException(id + "번 상품을 찾을 수 없습니다."));
         }
 
         // 상품 등록
@@ -41,7 +46,7 @@ public class ProductService {
         // 상품 삭제
         public void deleteProduct(int id) {
             Product product = productRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException(id + "번 상품을 찾을 수 없습니다."));
+                    .orElseThrow(() -> new NoSuchElementException(id + "번 상품을 찾을 수 없습니다."));
             productRepository.delete(product);
         }
 
