aa0f3dfc664c5310af0271a4a20b117ecec80508
dlawhd <whdvkf5139@naver.com>
2025-07-21 07:08:22 +0900

    사진, logo 클릭시 이동
    - 로그인 x -> localhost:3000
    - 로그인 o -> localhost:3000/products/list
    - Caffe 메뉴 서비스 입니다. -> Team | 12로 변경해두었습니다. 더 적절한 문구가 있으시면 언제든 말씀해 주세요! 우선 급하게 작성했습니다.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,10 @@
 export default function Page() {
   return (
     <>
-      <div className="flex-1 flex items-center justify-center text-white">
-        <span>안녕하세요. 카페 메뉴 서비스입니다.</span>
+      <div className="flex-1 flex items-center justify-center text-white mt-14">
+        <span className="text-3xl font-bold">
+          안녕하세요. 카페 메뉴 서비스 Team | 12 입니다.
+        </span>
       </div>
     </>
   );
